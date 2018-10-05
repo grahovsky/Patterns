909bd80fb24b64b297a31bd3011892263eb34259
Konstantin <grahovsky@gmail.com>
2018-10-05 21:07:00 +0300

    #порождающий #генерирует экземпляр для клиента, не раскрывая никакой логики

--- Pattern.playground/Contents.swift
+++ Pattern.playground/Contents.swift
@@ -1,1 +1,46 @@
 
+enum CarType {
+   case huge, fast
+}
+
+protocol Car {
+
+    func drive()
+
+}
+
+class HugeCar: Car {
+    
+    func drive() {
+        print("you drive huge car")
+    }
+    
+}
+
+class FastCar: Car {
+    
+    func drive() {
+        print("you drive fast car")
+    }
+}
+
+let hugeCar = HugeCar()
+hugeCar.drive()
+
+class FactoryCar {
+    
+    static func produceCar(type: CarType) -> Car {
+        
+        var car: Car
+        
+        switch type {
+        case .huge: car = HugeCar()
+        case .fast: car = FastCar()
+        }
+        return car
+    }
+    
+}
+
+let fastCar = FactoryCar.produceCar(type: .fast)
+fastCar.drive()
