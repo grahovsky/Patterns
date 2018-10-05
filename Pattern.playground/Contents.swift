19ad1c7c58a6a0c64b1283e4d6ecfba3541806dc
Konstantin <grahovsky@gmail.com>
2018-10-05 22:08:11 +0300

    #порождающий #обеспечивает тот факт, что создаваемый объект является единственным объектом своего класса

--- Pattern.playground/Contents.swift
+++ Pattern.playground/Contents.swift
@@ -1,1 +1,21 @@
 
+class Safe {
+
+    var money = 0
+    
+    static let shared = Safe()
+    
+    private init() {}
+    
+}
+
+
+let safeBefore = Safe.shared
+
+safeBefore.money += 200
+safeBefore.money += 100
+
+
+let safeAfter = Safe.shared
+safeAfter.money += 50
+print(safeAfter.money)
