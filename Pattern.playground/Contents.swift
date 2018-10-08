e37028c61311249407b0d50c4022eff87c161285
Konstantin <grahovsky@gmail.com>
2018-10-08 22:02:19 +0300

    #структурный #позволяет обернуть несовместимые объекты в адаптер, чтобы сделать их совместимыми с другим классом

--- Pattern.playground/Contents.swift
+++ Pattern.playground/Contents.swift
@@ -1,1 +1,43 @@
 
+// adaptee
+class SimpleCar {
+    
+    func sound() -> String {
+        return "tr-tr-tr-tr"
+    }
+}
+
+// target
+protocol SupercarProtocol {
+    
+    func makeNoise() -> String
+
+}
+
+class Supercar: SupercarProtocol {
+    
+    func makeNoise() -> String {
+        return "wroom-wroom"
+    }
+    
+}
+
+//adapter
+class SupercarAdapter: SupercarProtocol {
+    
+    var simpleCar: SimpleCar
+    
+    init(simpleCar: SimpleCar) {
+        self.simpleCar = simpleCar
+    }
+    
+    func makeNoise() -> String {
+        return simpleCar.sound()
+    }
+
+}
+
+let simpleCar = SimpleCar()
+let adapter = SupercarAdapter(simpleCar: simpleCar)
+
+adapter.makeNoise()
